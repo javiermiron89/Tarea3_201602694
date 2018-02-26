050dff036ff19aabaa77223b1e871f17ec59d289
Javier Mirón <Javier Mirn@192.168.1.101>
2018-02-25 21:18:45 -0600

    Tarea 3 inicializada

---
+++ src/tarea3_201602694/Tarea3_201602694.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3_201602694;
+
+/**
+ *
+ * @author Javier Mirón
+ */
+public class Tarea3_201602694 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.MenuPrincipal();
+    }
+    
+}
